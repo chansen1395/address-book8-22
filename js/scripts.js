baea60d3fadd2ca3a426ef2fce2c418237c68afd
Connor Hansen <chansen13@georgefox.edu>
2021-08-22 16:57:12 -0700

    add address book project files.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+let testContact = new Contact("Ada", "Lovelace", "503-555-1111");
