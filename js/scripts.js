bcd19d16e8836d2f9744104a069066cfdcc2bb9c
Connor Hansen <chansen13@georgefox.edu>
2021-08-24 12:00:58 -0700

    WIP remove empty address fields from displaying. Can't add them back for second full submits.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function AddressBook() {
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 };
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
 
-AddressBook.prototype.findContact = function(id) {
+AddressBook.prototype.findContact = function (id) {
   if (this.contacts[id] != undefined) {
     return this.contacts[id];
   }
   return false;
 };
 
-AddressBook.prototype.deleteContact = function(id) {
+AddressBook.prototype.deleteContact = function (id) {
   if (this.contacts[id] === undefined) {
     return false;
   }
@@ -38,12 +38,13 @@ function Contact(firstName, lastName, phoneNumber, bothAddresses) { // pretend a
   // this.address =  {home: address, email: address2}
 
 }
- // newContact => A new object using the Contact constructor
- // newContact = { firstName: "value", lastName: "value", phoneNumber: "value" }
+// newContact => A new object using the Contact constructor
+// newContact = { firstName: "value", lastName: "value", phoneNumber: "value" }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 };
+
 
 // User Interface Logic ---------
 let addressBook = new AddressBook();
@@ -51,47 +52,55 @@ let addressBook = new AddressBook();
 function displayContactDetails(addressBookToDisplay) {
   let contactsList = $("ul#contacts");
   let htmlForContactInfo = "";
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
     const contact = addressBookToDisplay.findContact(key);
     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
   });
   contactsList.html(htmlForContactInfo);
 }
 
-function showContact(contactId) {
-  const contact = addressBook.findContact(contactId);
-  $("#show-contact").show();
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
-  $(".address1").html(contact.bothAddresses.homeAddress);
-  $(".address2").html(contact.bothAddresses.emailAddress);
-  let buttons = $("#buttons");
-  buttons.empty();
-  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
-}
+
+  function showContact(contactId) {
+    const contact = addressBook.findContact(contactId);
+    if (contact.bothAddresses.homeAddress === "") {
+      $("#address1").remove();
+    } else if (contact.bothAddresses.homeAddress != "") {
+      $("#show-contact").append();
+    }
+
+    $("#show-contact").show();
+    $(".first-name").html(contact.firstName);
+    $(".last-name").html(contact.lastName);
+    $(".phone-number").html(contact.phoneNumber);
+    $(".address1").html(contact.bothAddresses.homeAddress);
+    $(".address2").html(contact.bothAddresses.emailAddress);
+    let buttons = $("#buttons");
+    buttons.empty();
+    buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+  }
+
 
 function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
+  $("ul#contacts").on("click", "li", function () {
     showContact(this.id);
   });
-  $("#buttons").on("click", ".deleteButton", function() {
+  $("#buttons").on("click", ".deleteButton", function () {
     addressBook.deleteContact(this.id);
     $("#show-contact").hide();
     displayContactDetails(addressBook);
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   attachContactListeners();
-  $("form#new-contact").submit(function(event) {
+  $("form#new-contact").submit(function (event) {
     event.preventDefault();
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedAddress = $("input#new-address").val();
     var inputtedAddress2 = $("input#new-address2").val();
-    var bothAddresses = {homeAddress: inputtedAddress, emailAddress: inputtedAddress2}
+    var bothAddresses = { homeAddress: inputtedAddress, emailAddress: inputtedAddress2 }
     // var bothAddresses = [inputtedAddress, inputtedAddress2];
     // bothAddresses.homeAddress <--- that would equal inputtedAddress
     $("input#new-first-name").val("");
